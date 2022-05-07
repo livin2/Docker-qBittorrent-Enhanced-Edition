4e2c91fc1ded2f799ba24e1b200bf0316449393e
livin2 <zxiphan@live.cn>
2022-05-07 17:42:06 +0800

    test

--- compile.sh
+++ compile.sh
@@ -33,18 +33,21 @@ cd /qbittorrent
 git clone https://github.com/c0re100/qBittorrent-Enhanced-Edition.git
 cd qBittorrent-Enhanced-Edition
 git checkout release-${QBV}
-./configure CXXFLAGS="-std=c++14" --disable-gui --disable-debug --with-boost-libdir=/usr/lib
+./configure CXXFLAGS="-std=c++14" --disable-gui --disable-debug --with-boost-libdir=/usr/lib64
 make clean && make -j$(nproc)
 make install
 
+ls -al /usr/
+ls -al /usr/lib/
+
 # packing qbittorrent
 ldd /usr/local/bin/qbittorrent-nox | cut -d ">" -f 2 | grep lib | cut -d "(" -f 1 | xargs tar -chvf /qbittorrent/qbittorrent.tar
-mkdir -p /qbittorrent/qbittorrent
-tar -xvf /qbittorrent/qbittorrent.tar -C /qbittorrent/qbittorrent
-cp --parents /usr/local/bin/qbittorrent-nox /qbittorrent/qbittorrent
-ls -al /qbittorrent/qbittorrent
-mkdir -p /qbittorrent/qbittorrent/usr
-mv /qbittorrent/qbittorrent/lib64 /qbittorrent/qbittorrent/usr/ || true
-mv /qbittorrent/qbittorrent/lib /qbittorrent/qbittorrent/usr/ || true
-ls -al /qbittorrent/qbittorrent/usr/
-ls -al /qbittorrent/qbittorrent/usrlocal/bin/ || echo "ls bin failed"
+# mkdir -p /qbittorrent/qbittorrent
+# tar -xvf /qbittorrent/qbittorrent.tar -C /qbittorrent/qbittorrent
+# cp --parents /usr/local/bin/qbittorrent-nox /qbittorrent/qbittorrent
+# ls -al /qbittorrent/qbittorrent
+# mkdir -p /qbittorrent/qbittorrent/usr
+# mv /qbittorrent/qbittorrent/lib64 /qbittorrent/qbittorrent/usr/ || true
+# mv /qbittorrent/qbittorrent/lib /qbittorrent/qbittorrent/usr/ || true
+# ls -al /qbittorrent/qbittorrent/usr/
+# ls -al /qbittorrent/qbittorrent/usrlocal/bin/ || echo "ls bin failed"
